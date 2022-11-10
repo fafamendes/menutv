ea0907315c3cf240957c0bca8563f5b1f37c15b4
Fabrício Mendes <fabricio.jmj@gmail.com>
2022-11-10 09:08:08 -0300

    Criação do layout da página de Slide e componente Produto

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,23 @@ const data = [
     products: [
       {
         name: 'Pão francês',
-        price: 'R$ 7,34'
+        price: 'R$ 7,34',
+        productImageUrl: 'https://i.pinimg.com/564x/00/e0/30/00e030619e352c815b47bd4447c40e5b.jpg'
       },
       {
         name: 'Sonho',
         price: 'R$ 3,00',
+        productImageUrl: 'https://i.pinimg.com/564x/f1/e7/fe/f1e7fe7f5200a722eec7339e32e0291b.jpg'
+      },
+      {
+        name: 'Donuts',
+        price: 'R$ 3,00',
+        productImageUrl: 'https://i.pinimg.com/564x/4c/2e/22/4c2e22f7ddf1ad1218433ebd612efd4c.jpg',
+      },
+      {
+        name: 'Pão Italiano',
+        price: 'R$ 17,99',
+        productImageUrl: 'https://i.pinimg.com/564x/36/37/82/363782e826bc019979a140239f4ef3a3.jpg'
       }
     ]
   },
--- src/features/slider/Slider.tsx
+++ src/features/slider/Slider.tsx
@@ -1,53 +1,53 @@
-import React, { ReactElement, useCallback, useEffect, useState } from 'react';
+import React, { ReactElement, useCallback, } from 'react';
 import { Splide, SplideTrack, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css';
 
+import '@splidejs/react-splide/css';
 import styles from './Slider.module.css';
 
-interface SliderProps {
+interface interfaceSliderProps {
   data?: any,
   delay?: number
 }
 
-interface Product {
+interface interfaceProduct {
   name: string,
+  description?: string,
   price: number,
   url?: string,
   productImageUrl?: string,
   qrCodePix?: string,
+  orientation: 'left' | 'right';
 }
 
-interface Slide {
+interface interfaceSlide {
   title: string,
-  products?: Product[]
+  products?: interfaceProduct[]
 }
 
-const { innerWidth: width, innerHeight: height } = window;
+const { innerHeight } = window;
 
-function Slider({ data = [], delay = 1000 }: SliderProps) {
+function Slider({ data = [], delay = 1000 }: interfaceSliderProps) {
 
   const drawSlides = useCallback(() => {
     let elements: ReactElement[] = []
 
-    data.map((slide: Slide, key: number) => {
-      elements.push(<SplideSlide key={`slide-${key}`}><SlidePage key={`slide-page-${key}`} title={slide.title} products={slide.products} /></SplideSlide>)
+    data.map((slide: interfaceSlide, key: number) => {
+      return elements.push(<SplideSlide key={`slide-${key}`}><SlidePage key={`slide-page-${key}`} title={slide.title} products={slide.products} /></SplideSlide>)
     });
     return elements;
   }, [data])
-
-
 
   return (
     <>
       <Splide hasTrack={false} options={{
         type: 'loop',
-        autoplay: true,
+        autoplay: false,
         pagination: false,
         arrows: false,
         speed: 500,
         interval: 5000
       }} className={`splide ${styles.splide}`}>
-        <SplideTrack>
+        <SplideTrack className={`${styles.track}`}>
           {drawSlides()}
         </SplideTrack>
       </Splide>
@@ -56,20 +56,42 @@ function Slider({ data = [], delay = 1000 }: SliderProps) {
 }
 
 
-function SlidePage({ title, products }: Slide, key: number) {
+function SlidePage({ title, products }: interfaceSlide, key: string) {
   return (
-    <div key={key} style={{ width }} >
-      <h1>{title}</h1>
-      {
-        products?.map((product: Product) =>
-          <div key={product.name}>
-            <h3>{product.name}</h3>
-            <div>
-              <span>{product.price}</span>
-            </div>
-          </div>)
-      }
-    </div >
+    <div className={styles.page} key={key}>
+      <div>
+        <h1>{title.toLocaleUpperCase()}</h1>
+        <div>
+          {
+            products?.map((product: interfaceProduct, key: number) =>
+              <Product
+                name={product.name.toUpperCase()}
+                price={product.price}
+                productImageUrl={product.productImageUrl}
+                orientation={key % 2 === 0 ? 'left' : 'right'}
+                key={key}
+              />
+            )
+          }
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Product({ name, price, orientation, productImageUrl }: interfaceProduct, key: number) {
+  console.log(orientation)
+  return (
+    <>
+      <div style={{ minHeight: ((innerHeight - innerHeight * 40 / 100) / 4) }} className={`${styles.product} ${styles[orientation]}`} key={name + '-' + key}>
+        {productImageUrl && <img style={{ maxHeight: ((innerHeight - innerHeight * 40 / 100) / 4) }} alt={name} src={productImageUrl} />}
+        <h3>{name}</h3>
+        <div className={styles.productDetails}>
+          <span>{price}</span>
+        </div>
+      </div >
+      <div className={styles.divider}></div>
+    </>
   )
 }
 
